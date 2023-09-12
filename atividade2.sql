627658dc86119c391b46c9fd9c95dcfcd9df780e
Francisco Ferreira <114003387+ferreirafrancisco0@users.noreply.github.com>
2023-09-12 08:18:48 -0300

    Update atividade2.sql

--- atividade2.sql
+++ atividade2.sql
@@ -2,6 +2,7 @@ CREATE TABLE clientes (
 codcliente INT(8)NOT NULL,
 nomecliente VARCHAR(50)NOT NULL,
 enderecocliente VARCHAR(40)NOT NULL,
+estado CHAR (2) NOT NULL, 
 cidade CHAR(2) NOT NULL,
 cep INT(8) NOT NULL,
 CHECK (cep > 1000),
@@ -11,10 +12,29 @@ PRIMARY KEY(codcliente)
 CREATE TABLE produtos (
 codproduto INT(8) NOT NULL,
 descproduto VARCHAR(50) NOT NULL,
-# pericivel BOOL NOT NULL,
-validade DATE NULL,
-detalhes VARCHAR (1000) NULL,
-fotodoproduto BLOB NULL,
-PRIMARY KEY codproduto,
- 
+pericivel BOOLEAN NOT NULL,
+validade DATE NOT NULL,
+detalhes VARCHAR(1000),
+fotodoproduto BLOB,
+PRIMARY KEY (codproduto)
 );
+
+
+CREATE TABLE vendas (
+codvenda INT (8) NOT NULL,
+codcliente INT (8) NOT NULL,
+PRIMARY KEY (codvenda, codcliente),
+FOREIGN KEY (codcliente) REFERENCES clientes(codcliente)
+);
+
+CREATE TABLE produtos_venda (
+codvenda INT(8) NOT NULL,
+codcliente INT(8) NOT NULL,
+codproduto INT(8) NOT NULL,
+quant DECIMAL(10, 3) NOT NULL,
+PRIMARY KEY (codvenda, codcliente, codproduto),
+CHECK (quant > 0),
+FOREIGN KEY (codvenda, codcliente) REFERENCES vendas(codvenda, codcliente),
+FOREIGN KEY (codproduto) REFERENCES produtos(codproduto)
+);
+
